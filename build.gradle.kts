ffb517459b4c6d9911c0fa54ffc38c552174f30a
Helltar <iam@helltar.com>
2023-01-24 17:18:08 +0200

    update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,19 +14,24 @@ repositories {
 }
 
 dependencies {
-    implementation("ch.qos.logback:logback-classic:1.4.5")
-    implementation("com.github.kittinunf.fuel:fuel-gson:2.3.1")
-    implementation("com.github.kittinunf.fuel:fuel:2.3.1")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
-    implementation("com.squareup.okhttp3:okhttp:4.10.0")
     implementation("io.github.kotlin-telegram-bot.kotlin-telegram-bot:telegram:6.0.7")
+
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.6.4")
+
+    implementation("com.github.kittinunf.fuel:fuel:2.3.1")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+
     implementation("org.json:json:20220924")
-    implementation("org.slf4j:slf4j-api:2.0.6")
+    implementation("com.github.kittinunf.fuel:fuel-gson:2.3.1")
     implementation("com.googlecode.json-simple:json-simple:1.1.1")
+
     implementation("org.xerial:sqlite-jdbc:3.40.0.0")
     implementation("org.jetbrains.exposed:exposed-core:0.40.1")
     implementation("org.jetbrains.exposed:exposed-jdbc:0.40.1")
+
+    implementation("org.slf4j:slf4j-api:2.0.6")
+    implementation("ch.qos.logback:logback-classic:1.4.5")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
 
     testImplementation(kotlin("test"))
 }
