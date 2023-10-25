d76ff70b0a8e5e72da0dee973bd99a813c68333c
Helltar <iam@helltar.com>
2023-10-25 19:30:20 +0300

    refact. AsrWhisper

--- src/main/kotlin/com/helltar/aibot/commands/user/audio/AsrWhisper.kt
+++ src/main/kotlin/com/helltar/aibot/commands/user/audio/AsrWhisper.kt
@@ -11,9 +11,7 @@ import com.helltar.aibot.utils.NetworkUtils.httpUpload
 import org.json.JSONException
 import org.json.JSONObject
 import org.slf4j.LoggerFactory
-import org.telegram.telegrambots.meta.api.objects.Video
-import org.telegram.telegrambots.meta.api.objects.VideoNote
-import org.telegram.telegrambots.meta.api.objects.Voice
+import org.telegram.telegrambots.meta.api.objects.*
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException
 import java.io.File
 import java.util.*
@@ -21,12 +19,19 @@ import java.util.concurrent.TimeUnit
 
 class AsrWhisper(ctx: MessageContext) : BotCommand(ctx) {
 
-    private val tempDir = System.getProperty("java.io.tmpdir")
-    private val log = LoggerFactory.getLogger(javaClass)
-
     private companion object {
-        const val MAX_AUDIO_FILE_SIZE = 25600 * 1000
+        const val WHISPER_MAX_AUDIO_FILE_SIZE = 25600000
     }
+
+    private val isCreator = isCreator()
+    private val tempDir = System.getProperty("java.io.tmpdir")
+
+    private val maxAudioSize = if (!isCreator) 1024000 else WHISPER_MAX_AUDIO_FILE_SIZE
+    private val maxVoiceSize = maxAudioSize
+    private val maxVideoDuration = if (!isCreator) 60 else 600
+    private val maxVideoNoteDuration = if (!isCreator) 90 else 900
+
+    private val log = LoggerFactory.getLogger(javaClass)
 
     override fun run() {
         if (isNotReply) {
@@ -34,34 +39,16 @@ class AsrWhisper(ctx: MessageContext) : BotCommand(ctx) {
             return
         }
 
-        val replyMessage = this.replyMessage ?: return
+        val fileData =
+            checkMediaTypeAndGetFileData(replyMessage!!)
+                ?: run {
+                    replyToMessage(Strings.VIDEO_OR_AUDIO_NOT_FOUND)
+                    return
+                }
 
-        if (!replyMessage.hasVoice() && !replyMessage.hasVideo() && !replyMessage.hasVideoNote()) {
-            replyToMessage(Strings.VIDEO_OR_AUDIO_NOT_FOUND)
-            return
-        }
-
-        var isVideo = false
-
-        val fileId =
-            if (replyMessage.hasVoice())
-                checkVoiceAndGetFileId(replyMessage.voice)
-            else {
-                isVideo = true
-
-                if (replyMessage.hasVideo())
-                    checkVideoAndGetFileId(replyMessage.video)
-                else
-                    if (replyMessage.hasVideoNote())
-                        checkVideoNoteAndGetFileId(replyMessage.videoNote)
-                    else
-                        null
-            }
-
-        fileId ?: return
-
-        val fileExt = if (isVideo) "mp4" else "wav"
-        var outFile = File("$tempDir/${UUID.randomUUID()}.$fileExt")
+        val fileId = fileData.first?.first ?: return
+        val fileName = fileData.first?.second ?: return
+        var outFile = File("$tempDir/${UUID.randomUUID()}_${fileName}")
 
         try {
             ctx.sender.downloadFile(Methods.getFile(fileId).call(ctx.sender), outFile)
@@ -69,6 +56,8 @@ class AsrWhisper(ctx: MessageContext) : BotCommand(ctx) {
             log.error(e.message)
             return
         }
+
+        val isVideo = fileData.second
 
         if (isVideo)
             outFile =
@@ -78,8 +67,8 @@ class AsrWhisper(ctx: MessageContext) : BotCommand(ctx) {
                         return
                     }
 
-        if (outFile.length() > MAX_AUDIO_FILE_SIZE) {
-            replyToMessage(Strings.AUDIO_MUST_BE_LESS_THAN.format("${MAX_AUDIO_FILE_SIZE / 1024000} mb."))
+        if (outFile.length() > WHISPER_MAX_AUDIO_FILE_SIZE) {
+            replyToMessage(Strings.VOICE_MUST_BE_LESS_THAN.format("${WHISPER_MAX_AUDIO_FILE_SIZE / 1024000} mb."))
             return
         }
 
@@ -97,7 +86,7 @@ class AsrWhisper(ctx: MessageContext) : BotCommand(ctx) {
             text.chunked(4096).forEach { replyToMessage(it, messageId) } // todo: 20 msg per min. (?)
 
         } catch (e: JSONException) {
-            log.error(e.message)
+            log.error("${e.message}: $responseJson")
             replyToMessage(Strings.CHAT_EXCEPTION)
         }
     }
@@ -105,40 +94,57 @@ class AsrWhisper(ctx: MessageContext) : BotCommand(ctx) {
     override fun getCommandName() =
         Commands.CMD_ASR
 
-    private fun checkVoiceAndGetFileId(voice: Voice): String? {
-        val maxVoiceSize = if (!isCreator()) 1024 * 1000 else MAX_AUDIO_FILE_SIZE
+    private fun checkMediaTypeAndGetFileData(message: Message): Pair<Pair<String, String>?, Boolean>? { // fileId, fileName, isVideo
+        if (message.hasVoice())
+            return Pair(checkVoiceAndGetFileData(message.voice), false)
 
-        return if (voice.fileSize <= maxVoiceSize)
-            voice.fileId
+        if (message.hasAudio())
+            return Pair(checkAudioAndGetFileData(message.audio), false)
+
+        if (message.hasVideo())
+            return Pair(checkVideoAndGetFileData(message.video), true)
+
+        if (message.hasVideoNote())
+            return Pair(checkVideoNoteAndGetFileData(message.videoNote), true)
+
+        return null
+    }
+
+    private fun checkAudioAndGetFileData(audio: Audio) =
+        if (audio.fileSize <= maxAudioSize)
+            Pair(audio.fileId, audio.fileName)
         else {
-            replyToMessage(Strings.AUDIO_MUST_BE_LESS_THAN.format("${maxVoiceSize / 1000} kb."))
+            replyToMessage(Strings.AUDIO_MUST_BE_LESS_THAN.format("${maxAudioSize / 1000} kb."))
             null
         }
-    }
 
-    private fun checkVideoAndGetFileId(video: Video): String? {
-        val maxVideoDuration = 60
+    private fun checkVoiceAndGetFileData(voice: Voice) =
+        if (voice.fileSize <= maxVoiceSize)
+            Pair(voice.fileId, "voice.ogg")
+        else {
+            replyToMessage(Strings.VOICE_MUST_BE_LESS_THAN.format("${maxVoiceSize / 1000} kb."))
+            null
+        }
 
-        return if (!isCreator() && video.duration > maxVideoDuration) {
+    private fun checkVideoAndGetFileData(video: Video) =
+        if (video.duration <= maxVideoDuration)
+            Pair(video.fileId, video.fileName)
+        else {
             replyToMessage(Strings.BAD_VIDEO_DURATION.format(maxVideoDuration))
             null
-        } else
-            video.fileId
-    }
+        }
 
-    private fun checkVideoNoteAndGetFileId(videoNote: VideoNote): String? {
-        val maxVideoNoteDuration = 90
-
-        return if (!isCreator() && videoNote.duration > maxVideoNoteDuration) {
+    private fun checkVideoNoteAndGetFileData(videoNote: VideoNote) =
+        if (videoNote.duration <= maxVideoNoteDuration)
+            Pair(videoNote.fileId, "video_note.mp4")
+        else {
             replyToMessage(Strings.BAD_VIDEO_NOTE_DURATION.format(maxVideoNoteDuration))
             null
-        } else
-            videoNote.fileId
-    }
+        }
 
-    private fun extractAudioFromVideo(videoFile: File): File? {
+    private fun extractAudioFromVideo(file: File): File? {
         val outputFilename = "$tempDir/audio_${UUID.randomUUID()}.wav"
-        val ffmpegCommand = "ffmpeg -i ${videoFile.absolutePath} -acodec pcm_s16le -ac 1 -ar 16000 $outputFilename"
+        val ffmpegCommand = "ffmpeg -i ${file.absolutePath} -acodec pcm_s16le -ac 1 -ar 16000 $outputFilename"
 
         return try {
             Runtime.getRuntime().exec(ffmpegCommand).waitFor(2, TimeUnit.MINUTES)
