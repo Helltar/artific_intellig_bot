00465648418b03980620da715422962d4368bc3d
Helltar <iam@helltar.com>
2024-04-20 17:04:27 +0300

    mv. CHAT_GPT_MODEL_4 to ChatGPTData

--- src/main/kotlin/com/helltar/aibot/commands/user/chat/models/ChatGPTData.kt
+++ src/main/kotlin/com/helltar/aibot/commands/user/chat/models/ChatGPTData.kt
@@ -3,6 +3,8 @@ package com.helltar.aibot.commands.user.chat.models
 object ChatGPTData {
 
     const val CHAT_GPT_MODEL_3_5 = "gpt-3.5-turbo"
+    const val CHAT_GPT_MODEL_4 = "gpt-4-turbo"
+
     const val CHAT_ROLE_USER = "user"
     const val CHAT_ROLE_ASSISTANT = "assistant"
     const val CHAT_ROLE_SYSTEM = "system"
