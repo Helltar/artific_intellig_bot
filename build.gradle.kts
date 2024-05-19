0831fad97a4f1f923147e617cb78c6a8f254bcf0
Helltar <iam@helltar.com>
2024-05-19 14:01:33 +0300

    upgrade version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-    kotlin("jvm") version "1.9.23"
+    kotlin("jvm") version "1.9.24"
     application
 }
 
 group = "com.helltar"
-version = "0.9.7"
+version = "0.9.8"
 
 repositories {
     mavenCentral()
@@ -12,19 +12,22 @@ repositories {
 
 dependencies {
     implementation("com.annimon:tgbots-module:7.1.0")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
     implementation("com.github.kittinunf.fuel:fuel:2.3.1")
     implementation("com.google.code.gson:gson:2.10.1")
+    implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
     implementation("org.imgscalr:imgscalr-lib:4.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.8.1")
     implementation("org.json:json:20231013")
-    implementation("org.slf4j:slf4j-api:2.0.6")
-    implementation("ch.qos.logback:logback-classic:1.4.12")
 
+    implementation("org.xerial:sqlite-jdbc:3.44.0.0")
     implementation("org.jetbrains.exposed:exposed-core:0.44.1")
     implementation("org.jetbrains.exposed:exposed-java-time:0.44.1")
     implementation("org.jetbrains.exposed:exposed-jdbc:0.44.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.7.3")
-    implementation("org.xerial:sqlite-jdbc:3.44.0.0")
+
+    implementation("org.slf4j:slf4j-api:2.0.6")
+    implementation("ch.qos.logback:logback-classic:1.4.12")
 }
 
 application {
