011a887f6043a9be369e5c3e683765eb678166ee
Helltar <iam@helltar.com>
2024-08-10 19:57:11 +0300

    rm. postgresql_driver_version var.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 val exposed_version: String by project
-val postgresql_driver_version: String by project
 
 plugins {
     kotlin("jvm") version "2.0.0"
@@ -28,13 +27,12 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.8.1")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
 
-    implementation("org.postgresql:postgresql:$postgresql_driver_version")
+    implementation("org.postgresql:postgresql:42.6.0")
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-java-time:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
 
-    implementation("org.slf4j:slf4j-api:2.0.13")
     implementation("ch.qos.logback:logback-classic:1.5.6")
 }
 
