cd89cc5ee6ef74116c80803c36c7b64b27435c84
Helltar <iam@helltar.com>
2024-08-19 19:02:06 +0300

    refactor: enhance admin list formatting
    - extracted list formatting logic into separate methods

--- src/main/kotlin/com/helltar/aibot/commands/admin/sudoers/AdminList.kt
+++ src/main/kotlin/com/helltar/aibot/commands/admin/sudoers/AdminList.kt
@@ -5,18 +5,21 @@ import com.helltar.aibot.Strings
 import com.helltar.aibot.commands.BotCommand
 import com.helltar.aibot.commands.Commands
 import com.helltar.aibot.db.dao.sudoersDao
+import com.helltar.aibot.db.models.SudoersData
 
 class AdminList(ctx: MessageContext) : BotCommand(ctx) {
 
     override suspend fun run() {
-        val list =
-            sudoersDao.getList().joinToString("\n") {
-                "<code>${it.userId}</code> <b>${it.username}</b> <i>(${it.datetime})</i>"
-            }
-
+        val list = getFormattedSudoersList(sudoersDao.getList())
         replyToMessage(list.ifEmpty { Strings.LIST_IS_EMPTY })
     }
 
     override fun getCommandName() =
         Commands.CMD_ADMIN_LIST
+
+    private fun getFormattedSudoersList(sudoers: List<SudoersData>) =
+        sudoers.joinToString("\n") { sudoer -> formatSudoer(sudoer) }
+
+    private fun formatSudoer(sudoer: SudoersData) =
+        "<code>${sudoer.userId}</code> <b>${sudoer.username}</b> <i>(${sudoer.datetime})</i>"
 }
