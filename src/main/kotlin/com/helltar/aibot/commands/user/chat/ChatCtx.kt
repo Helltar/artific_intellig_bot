a3b4454ea897e3b8a87238a55da64fbe25ede54b
Helltar <iam@helltar.com>
2024-08-19 19:08:02 +0300

    refactor ChatCtx
    - extract user id retrieval to a separate function
    - extract user chat history formatting to a separate function

--- src/main/kotlin/com/helltar/aibot/commands/user/chat/ChatCtx.kt
+++ src/main/kotlin/com/helltar/aibot/commands/user/chat/ChatCtx.kt
@@ -12,31 +12,15 @@ class ChatCtx(ctx: MessageContext) : BotCommand(ctx) {
     private val log = LoggerFactory.getLogger(javaClass)
 
     override suspend fun run() {
-        val userId =
-            if (!isReply) this.userId
-            else {
-                if (isAdmin()) message.replyToMessage.from.id
-                else {
-                    replyToMessage(Strings.ADMIN_ONLY_COMMAND)
-                    return
-                }
-            }
+        val userId = getUserId() ?: return
 
-        if (isCreator(userId))
-            if (!isCreator()) {
-                replyToMessage(Strings.CREATOR_CONTEXT_CANNOT_BE_VIEWED)
-                return
-            }
+        if (isCreator(userId) && !isCreator()) {
+            replyToMessage(Strings.CREATOR_CONTEXT_CANNOT_BE_VIEWED)
+            return
+        }
 
         val userChatHistory = ChatHistoryManager(userId).userChatDialogHistory
-
-        val text =
-            if (userChatHistory.isNotEmpty()) {
-                userChatHistory
-                    .filter { it.role == Chat.CHAT_ROLE_USER }
-                    .joinToString("\n") { "- ${it.content}" }
-            } else
-                Strings.CHAT_CONTEXT_EMPTY
+        val text = formatUserChatHistory(userChatHistory)
 
         try {
             replyToMessage(text, markdown = true)
@@ -48,4 +32,24 @@ class ChatCtx(ctx: MessageContext) : BotCommand(ctx) {
 
     override fun getCommandName() =
         Commands.CMD_CHATCTX
+
+    private suspend fun getUserId() =
+        if (!isReply)
+            this.userId
+        else {
+            if (isAdmin())
+                message.replyToMessage.from.id
+            else {
+                replyToMessage(Strings.ADMIN_ONLY_COMMAND)
+                null
+            }
+        }
+
+    private fun formatUserChatHistory(userChatHistory: List<Chat.MessageData>) =
+        if (userChatHistory.isNotEmpty()) {
+            userChatHistory
+                .filter { it.role == Chat.CHAT_ROLE_USER }
+                .joinToString("\n") { "- ${it.content}" }
+        } else
+            Strings.CHAT_CONTEXT_EMPTY
 }
