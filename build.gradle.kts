7a8504cf0b74fb4b301c875d746e4278a952b773
Helltar <iam@helltar.com>
2024-08-19 03:05:26 +0300

    update dependencies and refactor version management
    - move dependency versions to an object for centralized management
    - remove redundant properties in gradle.properties
    - update `build.gradle.kts` to reference the new version constants

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-val exposed_version: String by project
-
 plugins {
     kotlin("jvm") version "2.0.0"
     kotlin("plugin.serialization") version "2.0.0"
@@ -14,26 +12,37 @@ repositories {
     mavenCentral()
 }
 
+object Versions {
+    const val tgbotsModule = "7.7.0"
+    const val jacksonModuleKotlin = "2.14.2"
+    const val fuel = "2.3.1"
+    const val dotenvKotlin = "6.4.1"
+    const val imgscalrLib = "4.2"
+    const val exposedVersion = "0.52.0"
+    const val postgresql = "42.6.0"
+    const val coroutinesCoreJvm = "1.8.1"
+    const val kotlinxSerializationJson = "1.7.1"
+    const val logbackClassic = "1.5.6"
+}
+
 dependencies {
-    implementation("com.annimon:tgbots-module:7.7.0") {
-        exclude("org.telegram", "telegrambots-webhook")
-    }
+    implementation("com.annimon:tgbots-module:${Versions.tgbotsModule}") { exclude("org.telegram", "telegrambots-webhook") }
 
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
-    implementation("com.github.kittinunf.fuel:fuel:2.3.1")
-    implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
-    implementation("org.imgscalr:imgscalr-lib:4.2")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:${Versions.jacksonModuleKotlin}")
+    implementation("com.github.kittinunf.fuel:fuel:${Versions.fuel}")
+    implementation("io.github.cdimascio:dotenv-kotlin:${Versions.dotenvKotlin}")
+    implementation("org.imgscalr:imgscalr-lib:${Versions.imgscalrLib}")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.8.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
+    implementation("org.jetbrains.exposed:exposed-core:${Versions.exposedVersion}")
+    implementation("org.jetbrains.exposed:exposed-dao:${Versions.exposedVersion}")
+    implementation("org.jetbrains.exposed:exposed-java-time:${Versions.exposedVersion}")
+    implementation("org.jetbrains.exposed:exposed-jdbc:${Versions.exposedVersion}")
+    implementation("org.postgresql:postgresql:${Versions.postgresql}")
 
-    implementation("org.postgresql:postgresql:42.6.0")
-    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-java-time:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:${Versions.coroutinesCoreJvm}")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:${Versions.kotlinxSerializationJson}")
 
-    implementation("ch.qos.logback:logback-classic:1.5.6")
+    implementation("ch.qos.logback:logback-classic:${Versions.logbackClassic}")
 }
 
 application {
