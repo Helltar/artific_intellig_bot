a913a128a15fe6f7bb170d2eac2b0db04039506a
Helltar <iam@helltar.com>
2024-08-20 22:52:48 +0300

    refactor EnvConfig to use constants for env keys
    introduced constant keys for environment variables to improve code readability and maintainability. also changed function name from readEnv to readEnvVar for clarity

--- src/main/kotlin/com/helltar/aibot/EnvConfig.kt
+++ src/main/kotlin/com/helltar/aibot/EnvConfig.kt
@@ -4,18 +4,27 @@ import io.github.cdimascio.dotenv.dotenv
 
 object EnvConfig {
 
-    val creatorId = readEnv("CREATOR_ID").toLongOrNull() ?: throw IllegalArgumentException("bad CREATOR_ID env.")
+    private const val CREATOR_ID_KEY = "CREATOR_ID"
+    private const val BOT_TOKEN_KEY = "BOT_TOKEN"
+    private const val BOT_USERNAME_KEY = "BOT_USERNAME"
 
-    val telegramBotToken = readEnv("BOT_TOKEN")
-    val telegramBotUsername = readEnv("BOT_USERNAME")
+    private const val POSTGRESQL_HOST_KEY = "POSTGRESQL_HOST"
+    private const val POSTGRESQL_PORT_KEY = "POSTGRESQL_PORT"
+    private const val DATABASE_NAME_KEY = "DATABASE_NAME"
+    private const val DATABASE_USER_KEY = "DATABASE_USER"
+    private const val DATABASE_PASSWORD_KEY = "DATABASE_PASSWORD"
 
-    val postgresqlHost = readEnv("POSTGRESQL_HOST")
-    val postgresqlPort = readEnv("POSTGRESQL_PORT")
-    val databaseName = readEnv("DATABASE_NAME")
-    val databaseUser = readEnv("DATABASE_USER")
-    val databasePassword = readEnv("DATABASE_PASSWORD")
+    val creatorId = readEnvVar(CREATOR_ID_KEY).toLongOrNull() ?: throw IllegalArgumentException("Invalid value for $CREATOR_ID_KEY")
+    val telegramBotToken = readEnvVar(BOT_TOKEN_KEY)
+    val telegramBotUsername = readEnvVar(BOT_USERNAME_KEY)
 
-    private fun readEnv(env: String) =
-        dotenv { ignoreIfMissing = true }[env].ifBlank { throw IllegalArgumentException("$env env. is blank") }
-            ?: throw IllegalArgumentException("error when read $env env.")
+    val postgresqlHost = readEnvVar(POSTGRESQL_HOST_KEY)
+    val postgresqlPort = readEnvVar(POSTGRESQL_PORT_KEY)
+    val databaseName = readEnvVar(DATABASE_NAME_KEY)
+    val databaseUser = readEnvVar(DATABASE_USER_KEY)
+    val databasePassword = readEnvVar(DATABASE_PASSWORD_KEY)
+
+    private fun readEnvVar(env: String) =
+        dotenv { ignoreIfMissing = true }[env].ifBlank { throw IllegalArgumentException("$env environment variable is blank") }
+            ?: throw IllegalArgumentException("Error reading $env environment variable")
 }
