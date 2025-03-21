e5451c1f7827a68d61c19d71349fbbd522e2d2fd
Helltar <iam@helltar.com>
2025-03-21 20:43:21 +0200

    update kotlin version to 2.1.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.1.10"
-    kotlin("plugin.serialization") version "2.1.10"
+    kotlin("jvm") version "2.1.20"
+    kotlin("plugin.serialization") version "2.1.20"
     id("com.github.johnrengelman.shadow") version "8.1.1"
     application
 }
